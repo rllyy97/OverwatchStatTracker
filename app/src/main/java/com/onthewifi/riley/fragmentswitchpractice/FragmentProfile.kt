55943d570448af1b451608a1b272990ea53938c8
rllyy97 <rllyy97@gmail.com>
2018-07-05 21:07:44 -0500

    50% line now dotted

--- app/src/main/java/com/onthewifi/riley/fragmentswitchpractice/FragmentProfile.kt
+++ app/src/main/java/com/onthewifi/riley/fragmentswitchpractice/FragmentProfile.kt
@@ -1,9 +1,11 @@
 package com.onthewifi.riley.fragmentswitchpractice
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.DashPathEffect
+import android.graphics.Paint
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
-import android.support.constraint.ConstraintSet
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
@@ -36,13 +38,15 @@ class FragmentProfile: Fragment(), SrInitDialog.OnInputListener {
 
     private lateinit var userPath: DatabaseReference
 
+    private lateinit var baseView: ConstraintLayout
+
     private lateinit var title : TextView
     private lateinit var graphInfo : TextView
     private lateinit var graphInfoTail : TextView
     private lateinit var heroImage : ImageView
     private lateinit var graph : SparkView
+    private val newAdapter: GraphAdapter = GraphAdapter()
     private lateinit var lowMatchWarning : TextView
-    private lateinit var fiftyPercentView : TextView
 
     private lateinit var srGraphButton : Button
     private lateinit var wrGraphButton : Button
@@ -58,13 +62,22 @@ class FragmentProfile: Fragment(), SrInitDialog.OnInputListener {
         parent = activity as MainActivity
         userPath = parent.databaseRef.child("users").child(parent.user!!.uid)
 
+        baseView = view.findViewById(R.id.baseView)
         title = view.findViewById(R.id.titleTextView)
         graphInfo = view.findViewById(R.id.graphInfo)
         graphInfoTail = view.findViewById(R.id.graphInfoTail)
         heroImage = view.findViewById(R.id.heroImage)
         lowMatchWarning = view.findViewById(R.id.lowMatchWarning)
-        fiftyPercentView = view.findViewById(R.id.fiftyPercent)
+
         graph = view.findViewById(R.id.mainGraph)
+        newAdapter.setBase(50F)
+        var paint = Paint()
+        paint.strokeCap = Paint.Cap.ROUND
+        paint.strokeWidth = 3F
+        paint.color = Color.GRAY
+        paint.style = Paint.Style.STROKE
+        paint.pathEffect = DashPathEffect(floatArrayOf(3F, 15F),0F)
+        graph.baseLinePaint = paint
         srGraphButton = view.findViewById(R.id.srGraphButton)
         wrGraphButton = view.findViewById(R.id.wrGraphButton)
         allGraphButton = view.findViewById(R.id.allGraphButton)
@@ -122,7 +135,6 @@ class FragmentProfile: Fragment(), SrInitDialog.OnInputListener {
     private fun initGraph(tab: Int, span: Int) {
         currentGraphTab = tab
         currentGraphSpan = span
-        val newAdapter = GraphAdapter()
         var color = 0
         val currentTime = Calendar.getInstance().timeInMillis
 
@@ -176,7 +188,6 @@ class FragmentProfile: Fragment(), SrInitDialog.OnInputListener {
             }
         }
 
-
         when (currentGraphTab) {
             0 -> { // SR
                 color = ContextCompat.getColor(parent.baseContext, R.color.colorAccent)
@@ -185,25 +196,14 @@ class FragmentProfile: Fragment(), SrInitDialog.OnInputListener {
                 graphInfo.text = parent.sr.toString()
                 graphInfoTail.text = getString(R.string.sr)
                 newAdapter.setBaseLineBoolean(false)
-                fiftyPercentView.alpha = 0.0F
             }
             1 -> { // Win Rate
                 newAdapter.setBaseLineBoolean(true)
-                newAdapter.setBase(50F)
                 color = ContextCompat.getColor(parent.baseContext, R.color.colorPrimary)
                 srGraphButton.alpha = 0.5F
                 wrGraphButton.alpha = 1F
                 graphInfo.text = "%.2f".format(parent.winRate*100F)
                 graphInfoTail.text = "%"
-                // Constrains 50% Indicator to baseline
-                val base = graph.baseline
-                val fiftyPercentId = fiftyPercentView.id
-                val constraintSet = ConstraintSet()
-                constraintSet.clone(this.view as ConstraintLayout)
-                constraintSet.connect(base, ConstraintSet.TOP, fiftyPercentId, ConstraintSet.BOTTOM, 8)
-                constraintSet.connect(base, ConstraintSet.LEFT, fiftyPercentId, ConstraintSet.LEFT, 0)
-                constraintSet.applyTo(this.view as ConstraintLayout)
-                fiftyPercentView.alpha = 1.0F
             }
         }
         newAdapter.setY(floatArray)
